1849be463ec299cf8326108cf8f1b97b66bde51d
Mustapha <tahirumustapha13@gmail.com>
2024-09-19 17:29:34 +0000

    Added new cors origin

--- server.js
+++ server.js
@@ -2,12 +2,16 @@ const express = require("express");
 const dotenv = require("dotenv").config();
 const dbConfig = require("./config/dbConfig");
 const userRoute = require("./route/userRoute");
-const cors = require('cors')
+const cors = require("cors");
 const app = express();
-app.use(cors({
-    origin: 'https://benabhealth.netlify.app', // Update with your React app URL
-  }));
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST"],
+  })
+);
 app.use(express.json());
+
 app.use("/api/user", userRoute);
 
 const port = process.env.PORT || 5000;
