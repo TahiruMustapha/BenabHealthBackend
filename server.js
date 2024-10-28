075e2878b5a8f18931229c3a220bc7bbdfb5b6a2
Mustapha <tahirumustapha13@gmail.com>
2024-10-28 08:39:44 +0000

    new changes

--- server.js
+++ server.js
@@ -4,12 +4,10 @@ const dbConfig = require("./config/dbConfig");
 const userRoute = require("./route/userRoute");
 const cors = require("cors");
 const app = express();
-app.use(
-  cors({
-    origin: "*",
-    methods: ["GET", "POST"],
-  })
-);
+app.use(cors({
+  origin: '*', 
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], 
+}));
 app.use(express.json());
 
 app.use("/api/user", userRoute);
